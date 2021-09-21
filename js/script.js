279c82a5eae7aaeb49e818d1d9c7358a3b6878ec
Dominik Krawczyk <dominix.kr@gmail.com>
2021-09-21 17:21:35 +0200

    add playerMove script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+    computerMove = 'nożyce';
+}else{
+    computerMove ='papier';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -5,14 +21,11 @@ console.log('Gracz wpisał: ' + playerInput);
 let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
-    playerMove = 'kamień';
-    else if(playerInput == '2'){
+  playerMove = 'kamień';
+}else if(playerInput == '2'){
     playerMove = 'nożyce';
-    }
-    else {
+}else{
     playerMove = 'papier';
-    }
-
 }
 
 printMessage('Twój ruch to: ' + playerMove);
