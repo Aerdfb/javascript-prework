a7414c65aa14c669e286ccb6a3674d12e11a6f30
Dominik Krawczyk <dominix.kr@gmail.com>
2021-09-23 11:12:23 +0200

    add result function

--- js/script.js
+++ js/script.js
@@ -1,44 +1,45 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }else if(argMoveId == 2){
+        return 'nożyce';
+    }else if(argMoveId == 3){
+        return 'papier';
+    }
+  
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
 
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if(randomNumber == 2){
-    computerMove = 'nożyce';
-}else{
-    computerMove ='papier';
-}
-
+let computerMove = getMoveName(randomNumber)
 printMessage('Mój ruch to: ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: nożyce, 3: papier.');
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}else if(playerInput == '2'){
-    playerMove = 'nożyce';
-}else if(playerInput == '3'){
-    playerMove = 'papier';
-}
-else{
-    playerMove = 'nieznany ruch';
-}
-
+let playerMove = getMoveName(playerInput);
 printMessage('Twój ruch to: ' + playerMove);
 
-if(computerMove == 'kamień' && playerMove == 'papier') {
-    printMessage('Ty wygrywasz');
-}else if(computerMove == 'papier' && playerMove == 'nożyce') {
-    printMessage('Ty wygrywasz');
-}else if(computerMove == 'nożyce' && playerMove == 'kamień') {
-    printMessage('Ty wygrywasz');
-}else{
-    printMessage('Przegrywasz');
-}
+function displayResult(argComputerMove, argPlayerMove){
+    console.log('moves:', argComputerMove, argPlayerMove);
+    if(argComputerMove == getMoveName('1') && argPlayerMove == getMoveName('3')){
+        return 'Wygrywasz';
+    }else if(argComputerMove == getMoveName('3') && argPlayerMove == getMoveName('2')){
+        return 'Wygrywasz';
+    }else if(argComputerMove == getMoveName('2') && argPlayerMove == getMoveName('1')){
+        return 'Wygrywasz';
+    }else
+        return 'Przegrywasz';
+    }
+        
+    printMessage('Wynik rundy: ' + displayResult(computerMove, playerMove))
+    
+
+
+// printMessage('Wynik gry:' + displayResult)
+
+
+
